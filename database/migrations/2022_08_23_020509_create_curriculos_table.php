c3987f8dced4a0d8fa3d2c30920aaa93356c3164
Cleiton Lima <acrl.ribeiro17@gmail.com>
2022-08-22 23:38:18 -0300

    Criação da migration

---
+++ database/migrations/2022_08_23_020509_create_curriculos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('curriculo_id');
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('telefone');
+            $table->string('cargo_desejado');
+            $table->string('escolariade');
+            $table->text('observacoes')->nullable();
+            $table->string('arquivo');
+            $table->dateTime('data_cadastro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculos');
+    }
+};
