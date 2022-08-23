51eed5bd9935d00a682c247a7116231b247916dd
Cleiton Lima <acrl.ribeiro17@gmail.com>
2022-08-22 23:57:36 -0300

    Adição do controller, do form e da rota

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/curriculos', [CurriculoController::class, 'index'])->name('web.index');
